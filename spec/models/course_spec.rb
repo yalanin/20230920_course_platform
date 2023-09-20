38a17007e125a5fde86a5b3a8a1220caca9ad619
renee <yalanin@gmail.com>
2023-09-20 22:54:34 +0800

    fix describe scope

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   context 'validation' do
+    let(:course) { Course.new }
+
     describe 'name validate' do
-      let(:course) { Course.new }
-
       it 'name can not be empty' do
-        expect(course).not_to be_valid
-      end
-
-      it 'should belongs to a teacher' do
         expect(course).not_to be_valid
       end
 
       it 'should has a name error' do
         expect(course.valid?).to be false
         expect(course.errors.messages.key?(:name)).to be true
+      end
+    end
+
+    describe 'teacher validate' do
+      it 'should belongs to a teacher' do
+        expect(course).not_to be_valid
       end
 
       it 'should has a teacher relation error' do
